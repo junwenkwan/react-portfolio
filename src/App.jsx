e4c5a4a969cff4c5009d532ffa486be5618ac9a4
Jun Kwan <kwanjunwen@gmail.com>
2021-08-22 02:04:20 +1000

    Set up top bar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Topbar from "./components/topbar/Topbar"
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar/>
+      <div className="sections">
+        hello
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="wrapper">
+                <a href="#intro" className="logo">J. Kwan</a>
+            </div>
+        </div>
+    )
+}
