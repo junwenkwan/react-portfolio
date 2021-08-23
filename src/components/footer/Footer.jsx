ecc3efe7958c8be7244e1094aed60d7fc78d94cf
Jun Kwan <kwanjunwen@gmail.com>
2021-08-23 18:14:34 +1000

    Modify footer email

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="footer">
             <div className="wrapper">
-                <h3>For business inquiries: <br/> jkwan0001 at gmail dot com</h3>
+                <h3>For business inquiries: <br/> jkwan0001@gmail.com</h3>
             </div>
         </div>
     )
