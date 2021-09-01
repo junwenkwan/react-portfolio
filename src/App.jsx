0fd81a4986ea45c5d87c5608bf063e043e3e2120
Jun Kwan <kwanjunwen@gmail.com>
2021-09-01 20:42:41 +1000

    Integrate preloader to main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Preloader from "./components/preloader/Preloader"
 import Intro from "./components/intro/Intro"
 import About from "./components/about/About"
 import Work from "./components/work/Work"
@@ -5,18 +6,31 @@ import Projects from "./components/projects/Projects"
 import Skills from "./components/skills/Skills"
 import Footer from "./components/footer/Footer"
 import "./app.scss"
+import {useState, useEffect} from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setIsLoading(false);
+    }, 1500);
+  })
+
   return (
     <div className="app">
-      <div className="sections">
-        <Intro/>
-        <About/>
-        <Work/>
-        <Projects/>
-        <Skills/>
-        <Footer/>
-      </div>
+      {
+        isLoading === true?
+        <Preloader/>:
+        <div className="sections">
+          <Intro/>
+          <About/>
+          <Work/>
+          <Projects/>
+          <Skills/>
+          <Footer/>
+        </div>
+      }
+
     </div>
   );
 }
