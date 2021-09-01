9c1b12935d02d40fba4400a3ab9b8e7ef7e26306
Jun Kwan <kwanjunwen@gmail.com>
2021-09-01 18:43:30 +1000

    Minor change in about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
             <h1>About Me</h1>
             <div className="wrapper">
                 <div className="text">
-                    <p>&emsp; Hi, my name is Jun Kwan and I am passionate about technology. As an individual with strong interests in computer architecture, software development and machine learning, I studied Electrical and Computer Systems Engineering (ECSE) at Monash University. Upon my graduation in Jan 2021, I intended to pursue a career in software engineering because I would love to create and design a software product to have an impact on the society. During my free time, I enjoy reading and playing video games.</p>
+                    <p>&emsp; Hi, my name is Jun Kwan and I am passionate about technology. As an individual with strong interests in computer architecture, software and machine learning, I studied Electrical and Computer Systems Engineering (ECSE) at Monash University. Upon my graduation in Jan 2021, I intended to pursue a career in software engineering because I would love to create and design an innovative software product. During my free time, I enjoy reading and playing video games.</p>
                 </div>
             </div>
         </div>
