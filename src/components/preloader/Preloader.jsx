05ba2bd8904eaaa96c35a275a9d6d6b5f2c92c5f
Jun Kwan <kwanjunwen@gmail.com>
2021-09-02 01:18:15 +1000

    Beautify preloader with lottie file

--- src/components/preloader/Preloader.jsx
+++ src/components/preloader/Preloader.jsx
@@ -1,16 +1,24 @@
-import Loader from "react-loader-spinner";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Lottie from 'react-lottie';
+import * as animationData from './73810-business-idea-animation.json'
 
 export default function Preloader() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true, 
+        animationData: animationData.default,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice'
+        }
+      };
+
     return (
-        <div style={{width: "100vw", height:"50vh", display: "flex", alignItems : "center", justifyContent : "center"}}>
-            <Loader
-                type="Grid"
-                color="#15023a"
-                height={50}
-                width={50}
-                timeout={2000} //2 secs
-            />
+        <div style={{width: "100vw", height:"auto", display: "flex", flexDirection: "column", alignItems : "center", justifyContent : "center"}}>
+            <Lottie options={defaultOptions}
+              height={400}
+              width={400}/>
+            <p style={{color:'#15023a'}}>Animation by</p>
+            <p style={{color:'#15023a'}}>Mahendra Bhunwal</p>
+            
         </div>
     )
 }
