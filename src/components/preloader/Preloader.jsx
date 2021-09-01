f12d313ba9482e0ab647b03c63b5dde7ccb6d438
Jun Kwan <kwanjunwen@gmail.com>
2021-09-01 20:38:26 +1000

    Add preloader component

---
+++ src/components/preloader/Preloader.jsx
@@ -0,0 +1,16 @@
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+export default function Preloader() {
+    return (
+        <div style={{width: "100vw", height:"100vh", display: "flex", alignItems : "center", justifyContent : "center"}}>
+            <Loader
+                type="Grid"
+                color="#15023a"
+                height={50}
+                width={50}
+                timeout={2000} //2 secs
+            />
+        </div>
+    )
+}
