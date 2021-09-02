8d1ff343355e87efbb3b4c1d2c5305a18be09802
Jun Kwan <kwanjunwen@gmail.com>
2021-09-02 18:39:19 +1000

    Add credits to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,9 +4,13 @@ import MailIcon from '@material-ui/icons/Mail';
 export default function Footer() {
     return (
         <div className="footer">
+            <div className="credit">
+                <h3>Credits<br/>Rocket animation by Lex Komkov on LottieFiles<br/>Robot animation by Mikhail Voloshin on LottieFiles<br/>Typing animation by Santiago Pasciullo on LottieFiles</h3>
+            </div>
             <div className="wrapper">
+                
+                <h3>For business inquiries: jkwan0001@gmail.com</h3>
                 <MailIcon className='icon' style={{ color: 'white' }}/>
-                <h3>For business inquiries: <br/> jkwan0001@gmail.com</h3>
             </div>
         </div>
     )
