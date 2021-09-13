370805965ba00ef3423481ce82fa45cc48c80da5
Jun Kwan <kwanjunwen@gmail.com>
2021-09-13 22:18:51 +1000

    Beautify footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,21 @@
 import "./footer.scss"
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import MailIcon from '@material-ui/icons/Mail';
 
 export default function Footer() {
     return (
         <div className="footer">
             <div className="credit">
-                <h3>Credits<br/>Plant by Sheikh Sohel on LottieFiles<br/>Robot by Mikhail Voloshin on LottieFiles<br/>Typing by Santiago Pasciullo on LottieFiles</h3>
+                <h3>Credits</h3>
+                <h3>Plant by S. Sohel</h3>
+                <h3>Robot by M. Voloshin</h3>
+                <h3>Typing by S. Pasciullo</h3>
+                <h3>on LottieFiles</h3>
+                <h3>under CC BY 2.0</h3>
             </div>
-            <div className="wrapper">
-                
-                <h3>For business inquiries: jkwan0001@gmail.com</h3>
-                <MailIcon className='icon' style={{ color: 'white' }}/>
+            <div className="icon">
+                <a href={"https://www.linkedin.com/in/junwenkwan/"} target="_blank" rel="noreferrer"><LinkedInIcon className='linkedin' style={{ color: 'white' }}/></a>
+                <a href={"mailto:jkwan0001@gmail.com"} target="_blank" rel="noreferrer"><MailIcon className='mail' style={{ color: 'white' }}/></a>
             </div>
         </div>
     )
