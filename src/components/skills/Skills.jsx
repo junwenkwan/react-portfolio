278674463a5a1230e77ad422606472b8290a040a
Jun Kwan <kwanjunwen@gmail.com>
2021-09-15 19:10:37 +1000

    Rearrange icons in skills section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -15,28 +15,28 @@ export default function Skills() {
                     <h3>C</h3>
                 </div>
                 <div className="item">
-                    <img src="../assets/sql.svg" alt="" draggable="false"/>
-                    <h3>PostgreSQL</h3>
-                </div>
-                <div className="item">
                     <img src="../assets/git.svg" alt="" draggable="false"/>
                     <h3>Git</h3>
-                </div>
-                <div className="item">
-                    <img src="../assets/ubuntu.svg" alt="" draggable="false"/>
-                    <h3>Ubuntu</h3>
-                </div>
-                <div className="item">
-                    <img src="../assets/jupyter.svg" alt="" draggable="false"/>
-                    <h3>Jupyter</h3>
                 </div>
                 <div className="item">
                     <img src="../assets/opencv.svg" alt="" draggable="false"/>
                     <h3>OpenCV</h3>
                 </div>
                 <div className="item">
+                    <img src="../assets/ubuntu.svg" alt="" draggable="false"/>
+                    <h3>Ubuntu</h3>
+                </div>
+                <div className="item">
                     <img src="../assets/docker.svg" alt="" draggable="false"/>
                     <h3>Docker</h3>
+                </div>
+                <div className="item">
+                    <img src="../assets/jupyter.svg" alt="" draggable="false"/>
+                    <h3>Jupyter</h3>
+                </div>
+                <div className="item">
+                    <img src="../assets/sql.svg" alt="" draggable="false"/>
+                    <h3>PostgreSQL</h3>
                 </div>
             </div>
         </div>
